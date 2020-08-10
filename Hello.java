ff0b8683f438e7f24b9dbb006d39a94c48a188d6
ChanHo <avenew@naver.com>
2020-08-10 11:09:18 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+		public static void main(String[] arg){
+			System.out.println("Hello, welcome to the java world!");
+			System.out.println("Nice to meet you!");
+			int myAge = 28;
+			System.out.println("My Age is" + myAge);
+		}
+}
