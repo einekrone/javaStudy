0fe180b67170c76cfed367184c17cff600a318d1
ChanHo <avenew@naver.com>
2020-08-11 10:49:46 +0900

    ignore add

---
+++ VariableScopeExample.java
@@ -0,0 +1,10 @@
+public class VariableScopeExample {
+	public static void main(string[] args) {
+		int vi = 15;
+		int v2 = 0;
+		if(v1>10) {
+			int v2 = v1 - 10;
+		}
+		int v3 = v1 + v2 +5;
+	}
+}
